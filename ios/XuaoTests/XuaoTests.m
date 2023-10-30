4e67fcfa9f04b11898bb1cb410ac33cd88bb3861
nguyend3v <dvnguyen9x@gmail.com>
2023-10-30 11:32:42 +0700

    rename

--- ios/XuaoTests/XuaoTests.m
+++ ios/XuaoTests/XuaoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface LasSample023Tests : XCTestCase
+@interface XuaoTests : XCTestCase
 
 @end
 
-@implementation LasSample023Tests
+@implementation XuaoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
